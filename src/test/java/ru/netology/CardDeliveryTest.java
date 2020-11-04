ed19e139525f09141d810721c14c4a936538b8f0
alexreshetnikova <60314903+alexreshetnikova@users.noreply.github.com>
2020-11-04 19:10:19 +0600

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -14,24 +15,24 @@ public class CardDeliveryTest {
     String name = dataGenerator.getName();
     String phone = dataGenerator.getPhone();
     String city = dataGenerator.getCity();
-    String firstDate = dataGenerator.createDate(3);
-    String secondDate = dataGenerator.createDate(5);
 
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue(city);
-        form.$("[data-test-id=date] input").doubleClick().setValue(firstDate);
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(dataGenerator.createDate(3));
         form.$("[data-test-id=name] input").setValue(name);
         form.$("[data-test-id=phone] input").setValue(phone);
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(".notification_status_ok").shouldBe(exist);
-        form.$("[data-test-id=date] input").doubleClick().setValue(secondDate);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на "+ dataGenerator.createDate(3)));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(dataGenerator.createDate(7));
         form.$(".button").click();
         $("[data-test-id=replan-notification]").waitUntil(exist, 15000);
         $(withText("Перепланировать")).click();
         $(".notification_status_ok").shouldBe(exist);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на "+ dataGenerator.createDate(7)));
     }
 }
